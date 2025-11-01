4c4d8d0e9e3b704d0cb46837c55d14e2138692cc
Abdulrahman <syamabdo382@gmail.com>
2025-11-02 00:05:29 +0200

    Finish FloatingActionButton UI

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/notes_view_body.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_floating_action_button.dart';
 import 'package:note_app/widgets/custom_note_card.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,6 +10,7 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: CustomFloatingActionButton(),
       body: NotesViewBody(),
     );
   }
