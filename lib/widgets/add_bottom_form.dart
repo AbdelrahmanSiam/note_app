c987dc32a23797757e371d7a0b8655edd2efe59d
Abdulrahman <syamabdo382@gmail.com>
2025-11-18 18:21:45 +0200

    create AddNoteCubit and its states

---
+++ lib/widgets/add_bottom_form.dart
@@ -0,0 +1,65 @@
+
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_add_button.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
+
+class AddButtomForm extends StatefulWidget {
+  const AddButtomForm({
+    super.key,
+  });
+
+  @override
+  State<AddButtomForm> createState() => _AddButtomFormState();
+}
+
+class _AddButtomFormState extends State<AddButtomForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          CustomTextFormField(
+            hintText: 'Title',
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextFormField(
+            hintText: "Content",
+            maxLines: 6,
+            onSaved: (value) {
+              content = value;
+            },
+          ),
+          SizedBox(
+            height: 120,
+          ),
+          CustomAddButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          SizedBox(
+            height: 20,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widgets/custom_add_button.dart';
-import 'package:note_app/widgets/custom_text_field.dart';
+import 'package:note_app/widgets/add_bottom_form.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
@@ -10,67 +9,6 @@ class AddNoteButtomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SingleChildScrollView(child: AddButtomForm()),
-    );
-  }
-}
-
-class AddButtomForm extends StatefulWidget {
-  const AddButtomForm({
-    super.key,
-  });
-
-  @override
-  State<AddButtomForm> createState() => _AddButtomFormState();
-}
-
-class _AddButtomFormState extends State<AddButtomForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          CustomTextFormField(
-            hintText: 'Title',
-            onSaved: (value) {
-              title = value;
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextFormField(
-            hintText: "Content",
-            maxLines: 6,
-            onSaved: (value) {
-              content = value;
-            },
-          ),
-          SizedBox(
-            height: 120,
-          ),
-          CustomAddButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          SizedBox(
-            height: 20,
-          )
-        ],
-      ),
     );
   }
 }
