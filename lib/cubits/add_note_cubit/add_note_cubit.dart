da58af568e1f24f19f16b40a41d07037653e8fd7
Abdulrahman <syamabdo382@gmail.com>
2025-11-18 19:23:55 +0200

    Add addNote method

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
-  AddNoteCubit() : super(AddNoteInitial());
+  AddNoteCubit() : super(AddNoteInitialState());
+
+  addNote(NoteModel noteModel) async {
+    emit(AddNoteLoadingState());
+    try {
+      var noteBox = Hive.box<NoteModel>(notesBox);
+      emit(AddNoteSuccessState());
+      await noteBox.add(noteModel);
+    } catch (e) {
+      AddNoteFailureState(e.toString());
+    }
+  }
 }
--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -3,13 +3,13 @@ part of 'add_note_cubit.dart';
 @immutable
 sealed class AddNoteState {}
 
-final class AddNoteInitial extends AddNoteState {}
+final class AddNoteInitialState extends AddNoteState {}
 
-final class AddNoteLoading extends AddNoteState {}
+final class AddNoteLoadingState extends AddNoteState {}
 
-final class AddNoteSuccess extends AddNoteState {}
+final class AddNoteSuccessState extends AddNoteState {}
 
-final class AddNoteFailure extends AddNoteState {
+final class AddNoteFailureState extends AddNoteState {
   final String errMessage;
-  AddNoteFailure(this.errMessage);
+  AddNoteFailureState(this.errMessage);
 }
